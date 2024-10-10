cea4ba3f23bb0377dbfbbd0ad0726c6df3bc7343
Alexis Javier Sanabria Gaona <Alejavierpy@gmail.com>
2024-10-10 09:26:13 -0300

    feat(rutas): agregacion de la ruta de perosnas al servidor

--- server.ts
+++ server.ts
@@ -1,12 +1,20 @@
 import express, { Application } from "express";
+import * as personaRoutes from "./routes/persona";
 
 class Server {
   private app: Application;
   private port: string;
+  private apiPaths = {
+    personas: '/api/personas'
 
+  }
   constructor() {
     this.app = express();
     this.port = process.env.PORT || '8000';
+    this.routes();
+  }
+  routes() {
+    this.app.use(this.apiPaths.personas, personaRoutes.default);
   }
 
   listen() {
