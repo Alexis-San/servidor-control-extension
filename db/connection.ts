5f6fdebe4b1ded37721a2a7d984d465a1fc8b319
Alexis Javier Sanabria Gaona <Alejavierpy@gmail.com>
2024-10-23 07:47:10 -0300

    fix(connection): zona hoarario corregida

--- db/connection.ts
+++ db/connection.ts
@@ -3,6 +3,6 @@ import { Sequelize } from 'sequelize';
 const db =new Sequelize('control-extension-db','root','admin',{
     host:'localhost',
     dialect:'postgres',
-
+    timezone: '-03:00'
 });
 export default db;
