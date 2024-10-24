842111e7330d79f0c05f35f943949dfd5e3775de
Alexis Javier Sanabria Gaona <Alejavierpy@gmail.com>
2024-10-24 09:00:08 -0300

    feat(controller): agregado getPersonas y PutPersona

--- controllers/persona.ts
+++ controllers/persona.ts
@@ -1,34 +1,40 @@
 import { Request, Response } from 'express';
-import Persona from '../models/persona';
-
-
+import { createPersona, getAllPersonas } from '../services/persona';
 
 
 export const getPersonas = async (req: Request, res: Response) => {
-    const personas = await Persona.findAll();
-    res.json({
-        ok: true,
-        personas
-    });
-}
+    try {
+        const personas = await getAllPersonas();
+        
+        res.json({
+            ok: true,
+            personas
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            error: 'Error fetching personas'
+        });
+    }
+};
+
+
+export const postPersona = async (req: Request, res: Response) => {
+    try {
+        const persona = await createPersona(req.body);
+        res.json({
+            ok: true,
+            persona
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            error: 'Error creando persona'
+        });
+    }
+};
+
 /*
-export const getPersona=  (req: Request, res: Response) => {
-
-    res.json({
-        ok: true,
-        msg: 'getPersona'
-    });
-
-}
-
-export const postPersona=  (req: Request, res: Response) => {
-
-    res.json({
-        ok: true,
-        msg: 'getPersona'
-    });
-
-}
 export const putPersona=(req: Request, res: Response) => {
 
     res.json({
