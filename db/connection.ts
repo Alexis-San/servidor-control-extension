86cee2957c7189fa06b2e99254a823ba7e3bf8ef
Alexis Javier Sanabria Gaona <Alejavierpy@gmail.com>
2024-10-24 09:03:13 -0300

    feat(db): agregacion de una conexion remota

--- db/connection.ts
+++ db/connection.ts
@@ -1,8 +1,30 @@
-import { Sequelize } from 'sequelize'; 
+import { Sequelize } from 'sequelize';
 
-const db =new Sequelize('control-extension-db','root','admin',{
-    host:'localhost',
-    dialect:'postgres',
-    timezone: '-03:00'
+import 'dotenv/config';
+/*
+const db = new Sequelize('control-extension-db', 'root', 'admin', {
+  host: 'localhost',
+  dialect: 'postgres',
+  dialectOptions: {
+    useUTC: false, // for reading from database
+    dateStrings: true,
+    typeCast: true,
+    timezone: 'America/Asuncion',
+  },
+  timezone: 'America/Asuncion', // for writing to database
 });
+*/
+console.log(process.env.DB_URL);
+
+const db = new Sequelize(process.env.DB_URL||'', {
+  dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        supportBigNumbers: true,
+        ssl: {
+          rejectUnauthorized: false, // Trust the self-signed certificate
+        }
+    }
+});
+
 export default db;
