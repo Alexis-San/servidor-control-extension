80310419b76b3c7abb4cb8549161d696bebae17f
Alexis Javier Sanabria Gaona <Alejavierpy@gmail.com>
2024-10-24 09:02:14 -0300

    fix(routes): rutas get y post agregadas

--- routes/persona.ts
+++ routes/persona.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getPersonas } from "../controllers/persona";
+import { getPersonas, postPersona } from "../controllers/persona";
 
-//cimport { deletePersona, getPersona, getPersonas, postPersona, putPersona } from "../controllers/persona";
+
 const router= Router();
 
 router.get('/', getPersonas);
 //router.get('/:id',getPersona);
-//router.post('/',postPersona);
+router.post('/',postPersona);
 //router.put('/:id',putPersona);
 //router.delete('/:id',deletePersona);
 
