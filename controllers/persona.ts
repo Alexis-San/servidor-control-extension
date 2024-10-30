6f8c82e6ead467d428487f06ec07bd2d34ecdcdd
Alexis Javier Sanabria Gaona <Alejavierpy@gmail.com>
2024-10-30 08:05:26 -0300

    fix(endpoint): bug al hacer una busqueda y luego obtener todos los registros

--- controllers/persona.ts
+++ controllers/persona.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { createPersona, getAllPersonas, getPersonaById, updatePersona } from '../services/persona';
+import { createPersona, deletePersona, getAllPersonas, getPersonaById, updatePersona, checkExistingPersonaByCi, checkExistingPersonaByEmail } from '../services/persona';
 
 
 export const getPersonas = async (req: Request, res: Response) => {
@@ -23,38 +23,52 @@ export const getOnePersona = async (req: Request, res: Response) => {
         const { id } = req.params;
         const persona = await getPersonaById(Number(id));
         
-        if (!persona) {
+        if (persona) {
+            res.json({
+                ok: true,
+                persona
+            });
+        } else {
             res.status(404).json({
                 ok: false,
                 error: 'Persona not found'
             });
         }
-
-        res.json({
-            ok: true,
-            persona
-        });
     } catch (error) {
         res.status(500).json({
             ok: false,
             error: 'Error fetching persona'
         });
     }
-};
-
+}
 export const postPersona = async (req: Request, res: Response) => {
+    const { email, ci } = req.body;
+    const existingEmail = await checkExistingPersonaByEmail(email);
+    const existingCi = await checkExistingPersonaByCi(ci);
     try {
-        const persona = await createPersona(req.body);
-        res.json({
-            ok: true,
-            persona
-        });
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            error: 'Error creando persona'
-        });
-    }
+        if (existingEmail) {
+            res.status(400).json({
+                ok: false,
+                error: 'Email already exists'
+            });
+        } else if (existingCi) {
+            res.status(400).json({
+                ok: false,
+                error: 'CI already exists'
+            });
+        } else {
+            const newPersona = await createPersona(req.body);
+            res.json({
+                ok: true,
+                persona: newPersona
+            });
+        }
+    } catch(error) {
+    res.status(500).json({
+        ok: false,
+        error: 'Error creando persona'
+    });
+}
 };
 
 export const putPersona = async (req: Request, res: Response) => {
@@ -75,10 +89,10 @@ export const putPersona = async (req: Request, res: Response) => {
     }
 };
 
-export const deletePersona = async (req: Request, res: Response) => {
+export const deleteOnePersona = async (req: Request, res: Response) => {
     try {
         const { id } = req.params;
-        await updatePersona(Number(id), { estado: false });
+        await deletePersona(Number(id));
 
         res.json({
             ok: true,
